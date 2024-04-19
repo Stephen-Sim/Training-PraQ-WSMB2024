a72ef066baae18f041a392775908b95113cdf919
Stephen-Sim <simshansiong2002@gmail.com>
2024-04-19 17:02:45 +0800

    add moible

---
+++ Session2/MoibleApp/MoibleApp/MoibleApp/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MoibleApp.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MoibleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(usernameEntry.Text) || string.IsNullOrEmpty(passwordEntry.Text))
+            {
+                await DisplayAlert("Alert", "All fields are required.", "ok");
+                return;
+            }
+
+            if (usernameEntry.Text == "Alex" && passwordEntry.Text == "abc123")
+            {
+                await App.Current.MainPage.Navigation.PushAsync(new UserListPage());
+            }
+            else
+            {
+                await DisplayAlert("Alert", "user not found.", "ok");
+            }
+        }
+    }
+}
